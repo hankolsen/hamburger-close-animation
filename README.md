896917bfb7d268bb17fe021935cee02621a5a2f0
hank <henrik@hankolsen.com>
2016-10-29 19:47:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Inspired/stolen from http://www.materialup.com/posts/hamburger-close-icon-animation
+Implemented with pure css-animation.
---
+++ js/index.js
@@ -0,0 +1,4 @@
+var menu = document.getElementsByClassName('clicker')[0];
+menu.addEventListener('click', function(e){
+  menu.classList.toggle('active');
+});
